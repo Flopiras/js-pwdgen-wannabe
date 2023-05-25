32c9d920009ea90ac2fc57aab63430a921099714
Flopiras <Flopiras@yahoo.it>
2023-05-25 15:01:10 +0200

    add variables js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
 console.log('JS OK');
+
+//elemento DOM collegato
+const userPassword = document.getElementById('password')
+
+//nome utente
+const userName = prompt('Come ti chiami?' , 'Floriana')
+console.log(userName)
+
+//cognome utente
+const userSurname = prompt('Qual è il tuo cognome?' , 'Piras')
+console.log(userSurname)
+
+//colore preferito utente
+const userColor = prompt('Qual è il tuo colore preferito?' , 'Nero')
+console.log(userColor)
+
+//variabile saluto
+userPassword.innerText = userName + userSurname + userColor + 21
---
+++ steps.md
@@ -0,0 +1,8 @@
+**PASSWORD INSICURA**
+
+- recuperare l'elemento dal DOM
+- chiedere il nome all'utente (verificare che arrivi correttamente)
+- chiedere il cognome all'utente (verificare che arrivi correttamente)
+- chiedere il colore preferito all'utente (verificare che arrivi correttamente)
+- creare una variabile per la password
+- scrivere la password sulla pagina
